e592c5a28845c3fde2cd2b0958896380493e896e
Hyungtak Lee <28820367+hyungtaklee@users.noreply.github.com>
2026-01-11 21:18:29 -0600

    Add initial template for MTGP

---
+++ models/multi_task_gp.py
@@ -0,0 +1,25 @@
+import numpy as np
+import ..utils.func.
+
+# Set random seed for reproducibility
+np.seed(0)
+
+class MultiTaskGP:
+    """
+    Multi-task GP with Gaussian likelihood.
+    """
+
+    def __init__(self, train_x, train_y, likelihood):
+
+    # Run hyperparameter optimization w.r.t. evidence
+    def fit(self):
+
+    
+    # nagative log marginal likelihood, gradient for L-BFGS-B.
+    def nll_and_grad(self):
+
+
+
+    # 
+    def predict(self)
+
